a262ed44885e5bc523df5f8c25dcce5004a9f5d8
@Jeka Bayern <yevheniistepanchenko@gmail.com>
2020-11-09 19:43:57 +0200

    Pitch done

--- GameDev.cpp
+++ GameDev.cpp
@@ -2,10 +2,32 @@
 //
 
 #include <iostream>
+using namespace std;
+
+const int size_1 = 5;
+const int size_2 = 11;
+
+void show(int size_1, int size_2, char Array[5][11]) {
+	for (int i = 0; i < size_1; i++) {
+		for (int j = 0; j < size_2; j++) {
+			cout << Array[i][j] << ends;
+		}
+		cout << endl;
+	}
+}
+//int MoveToObject()
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	char Array[size_1][size_2]{
+	{'o','o','o','o','o','o','o','o','o','o','o'},
+	{'o','@','o','o','o','o','o','o','o','*','o'},
+	{'o','o','@','o','o','1','o','o','*','o','o'},
+	{'o','@','o','o','o','o','o','o','o','*','o'},
+	{'o','o','o','o','o','o','o','o','o','o','o'},
+	};
+
+	show(5, 11, Array);
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
